e53d362dad96183ba629b54fea5d5796ffcde100
Naz <nazariyv@users.noreply.github.com>
2021-09-17 18:32:39 +0100

    animetas subgraph snapshot (#7)
    * animetas subgraph snapshot
    
    * only lenderAddress is of interest
    
    * only animonkeys
    
    * max-line-length 88
    
    * max-line-length 88

---
+++ src/utils/subgraph_snapshot.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import requests
+import json
+from config import cfg
+
+url = cfg["Settings"]["renft_query_url"]
+
+
+# * run this script as python -m utils.subgraph_snapshot from src/
+
+# ! note that this only works for up to 1000 results
+# ! this is the graph protocol's limitation
+# ! to expand the set of results a combination of
+# ! of "first" and "skip" args in the query is
+# ! required
+def take_snapshot():
+    query = """
+    {
+      lendings(
+        orderBy: lenderAddress,
+        orderDirection: desc,
+        where: {nftAddress_in: ["0xa32422dfb5bf85b2084ef299992903eb93ff52b0"]}
+      ) {
+        lenderAddress
+      }
+    }
+    """
+    body = {"query": query}
+
+    res = requests.post(url, json=body)
+    res.raise_for_status()
+    lendings = map(lambda x: x["lenderAddress"], res.json()["data"]["lendings"])
+
+    with open("animetas_lenders_snapshot.json", "w+") as f:
+        f.write(json.dumps(list(lendings), indent=4))
+
+
+if __name__ == "__main__":
+    take_snapshot()
