df6da87b0d7a9865155f2d5e23a35f9e7dbdcf40
Sean D'Souza <sdammobubbles@gmail.com>
2021-11-09 06:36:09 -0500

    Add enabled flags as env variables for web server and bot (#13)

--- src/main.py
+++ src/main.py
@@ -46,18 +46,33 @@ for s in signals:
         s, lambda s=s: asyncio.create_task(shutdown(s, bot.loop)))
 port = int(os.getenv("LOCAL_API_PORT", "5000"))
 
+discord_bot_enabled = os.getenv(
+    "DISCORD_BOT_ENABLED", "True").lower() in ('true', '1', 't')
+web_server_enabled = os.getenv(
+    "WEB_SERVER_ENABLED", "True").lower() in ('true', '1', 't')
+
 try:
-    bot.loop.create_task(app.run_task(
-        host="0.0.0.0",
-        port=port,
-        certfile=os.getenv("CERT_FILE"),
-        keyfile=os.getenv("KEY_FILE")))
-    bot.run(os.environ.get("TOKEN"))
+    if web_server_enabled and discord_bot_enabled:
+        bot.loop.create_task(app.run_task(
+            host="0.0.0.0",
+            port=port,
+            certfile=os.getenv("CERT_FILE"),
+            keyfile=os.getenv("KEY_FILE")))
+        bot.run(os.environ.get("TOKEN"))
+    elif web_server_enabled and not discord_bot_enabled:
+        app.run(
+            host="0.0.0.0",
+            port=port,
+            certfile=os.getenv("CERT_FILE"),
+            keyfile=os.getenv("KEY_FILE"))
+    elif not web_server_enabled and discord_bot_enabled:
+        bot.run(os.environ.get("TOKEN"))
 except HTTPException:
     # restart on HTTP 429 Too Many Requests
     os.system("kill 1")
 except KeyboardInterrupt:
     logger.info("Process interrupted")
 finally:
-    bot.loop.close()
+    if discord_bot_enabled:
+        bot.loop.close()
     logger.info("Successfuly shutdown reNFT Animetas service")
